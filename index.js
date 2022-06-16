68cb1f4e275291db350018179a5a7f14f0144898
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-16 08:26:42 +0300

    commit tree 705d08effadc113113c54e2859c9a655e41d7a4c

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    });
+    
+function submitData(name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+
+      
+      body: JSON.stringify( {
+        name:"Steve",
+        email:"steve@steve.com",
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
